ebb511bb8194c31c48cdf50cf4ae31a7c74b9b85
Ivo Georgiev <ivogeorg@gmail.com>
2019-09-19 14:13:41 -0600

    Create new_program.js

---
+++ new_program.js
@@ -0,0 +1,5 @@
+// This is a comment. This is not parsed as code, just ignored.
+
+// This is where you paste the JS code from your Makecode editor, AFTER you have run the program on the micro:bit.
+
+// ....
